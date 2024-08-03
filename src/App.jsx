2f21ac0e77b45c0f1a692421db0f78d5fbdd82b3
Yelyzaveta Puzyrevska <169177255+Yelyzaveta-Puzyrevska@users.noreply.github.com>
2024-08-03 09:58:28 +0300

    смсмм

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <WelcomeSection />
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -0,0 +1,23 @@
+const WelcomeSection = () => {
+  return (
+    <section>
+      <h1>Welcome to React!</h1>
+      <button>Click</button>
+      <button>Login</button>
+      <button>Register</button>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia,
+        veritatis. Numquam earum, impedit vitae esse consequuntur velit omnis
+        facilis, excepturi architecto illo quod quae quasi ratione alias
+        corporis reprehenderit similique.
+      </p>
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+    </section>
+  );
+};
+export default WelcomeSection;
